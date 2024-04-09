bfb0432ab7e5731bc7b3456749696dd7ba7e60c9
Maks <46063054+mr1necs@users.noreply.github.com>
2024-04-10 01:55:51 +0300

    Create links.py

---
+++ app/links.py
@@ -0,0 +1,38 @@
+# ############################################### IMAGES ############################################### #
+
+image = ['AgACAgIAAxkBAAP8ZhW1W2IdBo_Q30_3BanZbR3TE8kAAl3eMRsKJbBIVr6Vv63oe2oBAAMCAAN5AAM0BA',
+         'AgACAgIAAxkBAAP-ZhW1YROGkHGbRMEEMEyjJFBi_PsAAl7eMRsKJbBITFprFqi-VNIBAAMCAAN5AAM0BA',
+         'AgACAgIAAxkBAAIBAAFmFbVmx01JEyW3n4HBKyUAAYHwY0EAAmHeMRsKJbBI9WgnyvJgHRsBAAMCAAN5AAM0BA',
+         'AgACAgIAAxkBAAIBAmYVtWrNuYpulBTCZ60TIAucOmixAAJi3jEbCiWwSJxdWLuFQhdeAQADAgADeQADNAQ',
+         'AgACAgIAAxkBAAIBBGYVtW6rLhYHC9JfKZCdZUhJOcrGAAJj3jEbCiWwSIPi7IWwhTdJAQADAgADeQADNAQ',
+         'AgACAgIAAxkBAAIBBmYVtXGjlW13J5cVH9COFrh3ToCWAAJk3jEbCiWwSPGNDYFLRUo2AQADAgADeQADNAQ',
+         'AgACAgIAAxkBAAIBCGYVtXUjaA_NzIT6JTKh170N_e0PAAJl3jEbCiWwSEbzNsw0lAZzAQADAgADeQADNAQ',
+         'AgACAgIAAxkBAAIBCmYVtXhIkFuJ-JJKvfGj6MZbu2JrAAJm3jEbCiWwSKWOVQthr21DAQADAgADeQADNAQ',
+         'AgACAgIAAxkBAAIBDGYVtXvRUtKo-GTTfU4fgacPG7c4AAJn3jEbCiWwSIyqsolPFAf0AQADAgADeQADNAQ',
+         'AgACAgIAAxkBAAIBDmYVtX40zx44qeocLZUD7lH8RtX1AAJo3jEbCiWwSGwDMSIZXGv1AQADAgADeQADNAQ']
+
+# ############################################### VIDEOS ############################################### #
+
+video = ['BAACAgIAAxkBAAIBIGYVuVAPEmzQH1m5g9B66NgOwfxQAAIxUAACCiWwSP9Z5R4pS__SNAQ',
+         'BAACAgIAAxkBAAIBImYVuXme1fqfWkmuY1E4AoFS2dVFAAIzUAACCiWwSDp0H6TXPW18NAQ',
+         'BAACAgIAAxkBAAIBJGYVuZaltql0fddSNtDiv9rS1ObnAAI0UAACCiWwSOpvzx8iwaxaNAQ',
+         'BAACAgIAAxkBAAIBJmYVucm-jjLpNlaqI0wdSsutiXQeAAI2UAACCiWwSBKOWShRKM4INAQ',
+         'BAACAgIAAxkBAAIBKGYVue9nmk22_bCcwUFYHDHZIsiRAAI3UAACCiWwSOjFZ-1GNJrcNAQ',
+         'BAACAgIAAxkBAAIBKmYVuhrDYTLOkXTmMQeNWXJNnwb2AAI4UAACCiWwSATA1CdzyrPRNAQ',
+         'BAACAgIAAxkBAAIBLGYVujdHJMp2yQyoeEzBQvAoCqEKAAI5UAACCiWwSFrGEW0LQSNPNAQ',
+         'BAACAgIAAxkBAAIBLmYVuk7mxjo8vNnNQXvHAQwHtQXkAAI6UAACCiWwSPmXN2KIwXrxNAQ',
+         'BAACAgIAAxkBAAIBMGYVuniY6P8aX0fncRBtaf_NpIbwAAI8UAACCiWwSPp3gvNZm0TkNAQ',
+         'BAACAgIAAxkBAAIBMmYVupOnh_dnRWnaDLPuqCKnBKzjAAI9UAACCiWwSJdFBy_KHZmINAQ']
+
+# ############################################### ARTICLE ############################################### #
+
+article = ['',
+           '',
+           '',
+           '',
+           '',
+           '',
+           '',
+           '',
+           '',
+           '']
