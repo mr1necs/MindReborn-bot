953d92206a090a5fe448b50574c0dabf4ffe2104
Maks <46063054+mr1necs@users.noreply.github.com>
2024-04-10 11:14:49 +0300

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,45 +1,46 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from app.links import article
 
 
-def keys(button1_teg, button2_teg=""):
-    if button2_teg != "":
+def keys(button_teg, button_url=""):
+    if button_url != "":
         result = InlineKeyboardMarkup(inline_keyboard=[[
-            InlineKeyboardButton(text='📖 ТЕКСТ', callback_data=button1_teg),
-            InlineKeyboardButton(text='📺 ВИДЕО', callback_data=button2_teg)
+            InlineKeyboardButton(text='📖 ТЕКСТ', url=button_url),
+            InlineKeyboardButton(text='📺 ВИДЕО', callback_data=button_teg)
         ]])
     else:
         result = InlineKeyboardMarkup(inline_keyboard=[[
-            InlineKeyboardButton(text='⬇️ ДАЛЕЕ', callback_data=button1_teg)
+            InlineKeyboardButton(text='⬇️ ДАЛЕЕ', callback_data=button_teg)
         ]])
 
     return result
 
 
-les_0 = keys("lesson0_article", "lesson0_video")
+les_0 = keys("lesson0_video", article[0])
 les_video_0 = keys("lesson1")
 
-les_1 = keys("lesson1_article", "lesson1_video")
+les_1 = keys("lesson1_video", article[1])
 les_video_1 = keys("lesson2")
 
-les_2 = keys("lesson2_article", "lesson2_video")
+les_2 = keys("lesson2_video", article[2])
 les_video_2 = keys("lesson3")
 
-les_3 = keys("lesson3_article", "lesson3_video")
+les_3 = keys("lesson3_video", article[3])
 les_video_3 = keys("lesson4")
 
-les_4 = keys("lesson4_article", "lesson4_video")
+les_4 = keys("lesson4_video", article[4])
 les_video_4 = keys("lesson5")
 
-les_5 = keys("lesson5_article", "lesson5_video")
+les_5 = keys("lesson5_video", article[5])
 les_video_5 = keys("lesson6")
 
-les_6 = keys("lesson6_article", "lesson6_video")
+les_6 = keys("lesson6_video", article[6])
 les_video_6 = keys("lesson7")
 
-les_7 = keys("lesson7_article", "lesson7_video")
+les_7 = keys("lesson7_video", article[7])
 les_video_7 = keys("lesson8")
 
-les_8 = keys("lesson8_article", "lesson8_video")
+les_8 = keys("lesson8_video", article[8])
 les_video_8 = keys("lesson9")
 
-les_9 = keys("lesson9_article", "lesson9_video")
+les_9 = keys("lesson9_video", article[9])
