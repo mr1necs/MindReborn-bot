15a7fb05ae79d8fc85e7138c0864136162d0986c
Maks <46063054+mr1necs@users.noreply.github.com>
2024-04-10 10:56:42 +0300

    Update parser.py

--- app/parser.py
+++ app/parser.py
@@ -1,6 +1,7 @@
 import gspread
+from app.config import CREDENTIALS
 
+gc = gspread.service_account_from_dict(CREDENTIALS)
 
-gc = gspread.service_account(
-    filename='/Users/mr1necs/Documents/GitHub/Testing-work-bot/app/testing-work-bot-8909f6be7ad6.json')
 sh = gc.open("Bot Data")
+worksheet = sh.sheet1
