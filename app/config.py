e54c624c140232304ec71dd63d754c3263ec9e2e
Maks <46063054+mr1necs@users.noreply.github.com>
2024-04-16 16:04:45 +0300

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,36 @@
+BOT_TOKEN = "6991049668:AAGfVTVzr_OrwNKOontsw-3f6GHBzRLKJF4"
+
+CREDENTIALS = {
+  "type": "service_account",
+  "project_id": "testing-work-bot",
+  "private_key_id": "8909f6be7ad6e73d28ce91834b66ca5aabf6aacb",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDhns4ZWcLy0rMc\nOntjim"
+                 "3OaH0U53Q2P9RBj5LvkVmRNQjDwjbDkc3n2sH9h8DkyvuZ9+1dnlWfFyGN\n0K63JxB3jcV2qSl1g34G6U+CypK0nqgE/nB24ApoX"
+                 "I+ngaP/NAeGJeR0kHg76hLa\no7VfKEfJaO4OaIm4PGp9jmECezpzYhgy0uCVYhdp00hui/VaxoVJqBjokuk1mFcL\nih"
+                 "+nF5Apc+ABOY/eJcluLM3mv+oxf5KNM7pFNnkhRAo0XondSvWpYdip9B7pHPNy\nGJdpLO/gf94M6eRCiyzCAFCzd2ev"
+                 "/lt4eSgQb4n4xSBqYI/srVJz/NWA8XoySooR\njC/JQq8VAgMBAAECggEAATDcpHXHaY8y"
+                 "+qeUs6WNJ1KrXa3MEfuRGVMn7Xjzrf3H\nllBDMuc6h5UCwvMRZv5q82ODiU6MOVBUBm2Ed+XY+HyTKdQXlS3KFOtsgUOkSxeZ"
+                 "\nn5nKildr4/mgbiapG4V7iFnRBHbSak81usQ0Rp84jnJYlqhhbmDD7MuQgloX/fho\ndMabc/A2hNJCdXztXEl5UnDs+Q081"
+                 "/sSr7Hc/XXZFAhViBIKR90zHpICfxOTsCxy\ngLdB5xu9zQ/7xtCdjoqRK4LE70Hf2qQlW3C2INn"
+                 "+VDHk4A20fjaD2EL46ulMXwJf\nMe5fXkK/q563IAoqZHAdp6X8237KA9kUbdIESX2eAQKBgQD6NTsHSkISiRg+S54y"
+                 "\naYETSgpEsXLNEGhG+P2/DLAHnmDICBbFMkbFGWP7Cb5E3Hc2IMsB9ycXRXbRD/7r"
+                 "\nVZcGu0qQ0QG64M3yboTNPsGm7gvAfX6WA9MzuHj6XOgeijoZoat0m7F20X0fzomS\na7Xvrj1y48Tx"
+                 "/UpiIt3gys5ZFQKBgQDm191eaMo0F9ggVxBo7ZH2CgpvZgZ4Xjv3"
+                 "\nFQpMQjXMGclZK1hu2eSUYcNoqYSQfJZiWLpouWLEnfsaVP05ZUKMIr31X1kNve6s\nda5hldKy0lexy"
+                 "+vrMu7xAAsPIauhexBk5CLlIGmDD86ivYtOte+7QtYDtqjLFMyj\n1+cHYyx+AQKBgQCYi2OVqjxmOJdDQ68LZS"
+                 "+Apx9jihxH0b0BqERr+jxIKNMwoV3g\nlCp5eGVwZBxQt2km4w9h/2Eh2wP5qhrtN52ASSfI6uR3Hx/wsljdGWnPcEpR7TEE"
+                 "\nHQji+5EmcwZBuFx7zAlQaqvjmQ0EG1PcpTajDkQAi8VDE/4JnuBPcNYlkQKBgHI2"
+                 "\nMT9pp0l41qTBjsvrZsENnLYbC254aUDJNywdwhI2W+lzSbCmu7IyYIUjAgSer6T+\n81OnDVfsNfWEQTkpgXBmLAf/4carxy"
+                 "+qldmQUAvLiDneg939eDzi4ZXd+dXOVoEk\nYqNskueWZY0dLJSH7ZF/8vUpZX/8zXbu9k4wYi4BAoGAOfK9/N"
+                 "+qMfocOJGQbEHd\n9t3Aup3i2OPxtG8wtgsqXLIHOAcmo6NHCpCQd6TqTgd65BXGDXlg+ZhY9+Hnwo8/\nIPu110VRcV7o"
+                 "/wkiqDtu16WZZnZksohBm6KLspXb35xd//A8sdkjY8WYckqgC+cr\na0iW/CffpEkG42M774w4/E0=\n-----END PRIVATE "
+                 "KEY-----\n",
+  "client_email": "testing-work-bot@testing-work-bot.iam.gserviceaccount.com",
+  "client_id": "112424510530636244594",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/testing-work-bot%40testing-work-bot.iam"
+                          ".gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
